3da328f5f7a861bd61f344d72b568a4f67fed2d4
tankz1776 <101753930+tankz1776@users.noreply.github.com>
2024-04-26 20:21:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+ document.getElementById("resume-button").addEventListener("click", function() {
+	window.location.href = "resume.html";
+});
+
+document.getElementById("projects-button").addEventListener("click", function() {
+	window.location.href = "projects.html";
+});
